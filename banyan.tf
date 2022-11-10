add9d9a2294d7cf8fdca1af7feff0821aec4b9cd
Matt Schiller <mschiller@banyansecurity.io>
2022-11-10 13:20:42 -0800

    update provider

--- banyan.tf
+++ banyan.tf
@@ -1,15 +1,10 @@
 terraform {
   required_providers {
     banyan = {
-      source  = "github.com/banyansecurity/banyan"
-      version = ">=0.9.0"
+      source  = "banyansecurity/banyan"
+      version = ">=0.9.1"
     }
   }
-}
-
-provider "banyan" {
-  api_key = var.api_key
-  host    = var.banyan_host
 }
 
 resource "banyan_accesstier" "accesstier" {
@@ -19,7 +14,7 @@ resource "banyan_accesstier" "accesstier" {
   disable_snat            = var.disable_snat
   src_nat_cidr_range      = var.src_nat_cidr_range
   api_key_id              = banyan_api_key.accesstier.id
-  tunnel_private_domains   = var.tunnel_private_domains
+  tunnel_private_domains  = var.tunnel_private_domains
   tunnel_cidrs            = var.tunnel_cidrs
   console_log_level       = var.console_log_level
   file_log_level          = var.file_log_level
