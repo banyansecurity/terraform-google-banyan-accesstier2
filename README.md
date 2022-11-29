5f365a5ad0b84ed5efa279422cec4f8f71e8d306
Matt Schiller <mschiller@banyansecurity.io>
2022-11-29 11:27:49 -0800

    bump version

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ terraform {
   required_providers {
     banyan = {
       source  = "banyansecurity/banyan"
-      version = "0.9.2"
+      version = "1.0"
     }
   }
 }
--- banyan.tf
+++ banyan.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     banyan = {
       source  = "banyansecurity/banyan"
-      version = ">=0.9.1"
+      version = ">=1.0"
     }
   }
 }
