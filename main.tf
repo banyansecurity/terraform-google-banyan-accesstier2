69aca801855baff15f81c79fb2347f82d4acba9c
Dan Hiris <danhiris@banyansecurity.io>
2023-02-07 17:40:17 -0700

    external network load balancers need to allow different CIDRs for health checks

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "google_compute_region_health_check" "backend_service_loadbalancer_heal
 }
 
 locals {
-  healthcheck_prober_ip_ranges = [35.191.0.0/16, 209.85.152.0/22, 209.85.204.0/22]
+  healthcheck_prober_ip_ranges = ["35.191.0.0/16", "209.85.152.0/22", "209.85.204.0/22"]
 }
 
 resource "google_compute_region_backend_service" "accesstier" {
